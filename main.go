7c4ac86f11893cf6f94ded02b2f80cc89e87eef3
Marcus Nilszén <mebn122@gmail.com>
2021-05-07 19:24:26 +0200

    did a simple webserver with routes to 2 pages

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	log.Println("Running...")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setupRoutes() {
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/room", roomPage)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./public/index.html")
+}
+
+func roomPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./public/room.html")
 }
