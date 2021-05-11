43583cab6a4c194bfd2065c5d393f2fd830c8144
Marcus Nilszén <mebn122@gmail.com>
2021-05-11 23:04:43 +0200

    added websockets for communication

--- room.go
+++ room.go
@@ -1,6 +1,18 @@
 package main
 
+import "log"
+
 type Room struct {
 	code    string
 	players map[*Player]*Player
+}
+
+func (room *Room) SendToRoom(msgType int, msg []byte) {
+	for _, client := range room.players {
+		err := client.conn.WriteMessage(msgType, msg)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
