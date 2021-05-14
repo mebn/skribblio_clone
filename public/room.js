950c664934a37cac72c794514eafa81f7a78900c
Marcus Nilszén <mebn122@gmail.com>
2021-05-14 19:26:07 +0200

    moved Draw.js code to room.js to have access to some functions

--- public/room.js
+++ public/room.js
@@ -44,6 +44,53 @@ const receiveOn = (name, msg, cb) => {
 
 
 
+// Draw.js
+function _(selector) {
+    return document.querySelector(selector);
+}
+
+function setup() {
+    let canvas = createCanvas(750, 700);
+    canvas.parent("canvas-wrapper");
+    background(255);
+    strokeWeight(10);
+}
+
+function resize(clicked_id) {
+    let size = 0;
+    if (clicked_id == "one") size = 10;
+    else if (clicked_id == "two") size = 20;
+    else if (clicked_id == "three") size = 30;
+    else size = 40;
+    strokeWeight(size);
+}
+
+function mouseDragged() {
+    let type = _("#pen-brush").checked ? "brush" : "eraser";
+    let color = _("#color-picker").value;
+
+    // Send data needed to draw and then do the actual
+    // drawing in socket.onmessage. OBS! there might be some delay
+    // on poor network connection.
+    let obj = { color, pmouseX, pmouseY, mouseX, mouseY };
+    let data = JSON.stringify(obj);
+    sendOn("drawing", data);
+
+    stroke(color);
+    if (type == "brush") {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    } else {
+        stroke(255);
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+_("#reset-canvas").addEventListener("click", function () {
+    background(255);
+});
+
+
+
 
 socket.onopen = () => {
     console.log("Connected to server!")
@@ -70,6 +117,7 @@ socket.onclose = err => {
 socket.onerror = err => {
     console.log("socket error: ", err)
 }
+
 
 document.getElementById("send").addEventListener("click", e => {
     const text = document.getElementById("text")
